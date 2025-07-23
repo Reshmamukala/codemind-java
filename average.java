af4ef7009b72276b93a3b72c052b593a983f8d34
Reshmamukala <22P31A1209@acet.ac.in>
2025-07-23 20:46:47 +0530

    new average

---
+++ average.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Average
+{
+    Scanner sc=new Scanner(System.in);
+    int a=sc.newInt();
+    int b=sc.newInt();
+    float c;
+    c=(a+b)/2.0;
+    System.out.printf("%.4f",c);
+}
